fecef4d30b0f063a693025aabddcd631bf207fa1
Ambushfall <lukathetormentor@gmail.com>
2023-01-03 15:05:04 +0100

    Modified

---
+++ src/routes/milos/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { confetti } from '@neoconfetti/svelte';
+	import { reduced_motion } from '../sverdle/reduced-motion';
+</script>
+
+<svelte:head>
+	<title>Milos</title>
+	<meta name="description" content="Webpage made for the one and only" />
+</svelte:head>
+<h1>Welcome to Milos's webpage</h1>
+<div
+	use:confetti={{
+		particleCount: $reduced_motion ? 0 : undefined,
+		force: 0.7,
+		stageWidth: window.innerWidth,
+		stageHeight: window.innerHeight,
+		colors: ['#ff3e00', '#40b3ff', '#676778']
+	}}
+/>
